26e92c88587d8fd945b03c96c0225ada3a0e9ab2
Karan Kaushik <kkricardokaka95@gmail.com>
2017-07-28 18:10:02 +0530

    Update new trial.php

--- new trial.php
+++ new trial.php
@@ -12,6 +12,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$indirecttime;
 	$lat3  = $_POST['Latitude']; //Third point Latitude
 	$long3 = $_POST['Longitude']; //Third point Longitude
+	$json = file_get_contents('https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyB6vwsPFlyQtLcW39ctJBKGn59yPGFU7Ew');//testing if api works 
+	$data = json_decode($json, true);
+	print_r ($data.rows[0].elements[0].distance.text);
+	
 }
 ?>
 
@@ -25,44 +29,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			Latitude: <input type="text" name="Latitude">&nbsp&nbsp&nbsp&nbsp&nbsp
 			Longitude: <input type="text" name="Longitude"><br><br>
 			<input type="submit">
-		</form>
-	<script>
-		var origin1 = new google.maps.LatLng("<?php echo $lat1 ?>", "<?php echo $long1 ?>");
-		var origin2 = new google.maps.LatLng("<?php echo $lat2 ?>", "<?php echo $long2 ?>");
-		var destinationA = new google.maps.LatLng("<?php echo $lat2 ?>", "<?php echo $long2 ?>");
-		var destinationB = new google.maps.LatLng("<?php echo $lat3 ?>", "<?php echo $long3 ?>");
-
-		var service = new google.maps.DistanceMatrixService();
-		service.getDistanceMatrix(
-		{
-			origins: [origin1, origin2],
-			destinations: [destinationA, destinationB],
-			travelMode: 'DRIVING',
-			drivingOptions:{departureTime: new Date(Date.now())},
-			unitSystem: google.maps.UnitSystem.METRIC,
-			avoidHighways: False,
-			avoidTolls: False,
-		}, callback);
-
-		function callback(response, status) {
-			if (status == 'OK') {
-			var origins = response.originAddresses;
-			var destinations = response.destinationAddresses;
-
-				for (var i = 0; i < origins.length; i++) {
-					var results = response.rows[i].elements;
-					for (var j = 0; j < results.length; j++) {
-						var element = results[j];
-						var distance = element.distance.text;
-						var duration = element.duration.text;
-						var from = origins[i];
-						var to = destinations[j];
-					}
-				}
-			}
-		}
-    </script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB6vwsPFlyQtLcW39ctJBKGn59yPGFU7Ew&callback=initMap">
-    </script>	
+		</form>	
 	</body>
 </html>
